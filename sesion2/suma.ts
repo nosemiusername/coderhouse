5e57c22d037686ab1f7a4fe75cf674297be6fcd6
nosemiusername <rafael@imatec.cl>
2021-03-20 21:01:20 -0300

    desafio clase2

---
+++ sesion2/suma.ts
@@ -0,0 +1,14 @@
+export class suma{
+    private number1;
+    private number2;
+
+    constructor(number1:number, number2:number){
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    resultado(){
+        return this.number1 + this.number2;
+    }
+     
+}
