e99c97ccc83a11264330ed029de303d832dbaa29
nosemiusername <rafael@imatec.cl>
2021-04-07 01:15:56 -0400

    se corrige la carga asincronica

--- sesion6/Archivo.ts
+++ sesion6/Archivo.ts
@@ -3,21 +3,22 @@ import fs from "fs";
 
 export class Archivo {
 
-    private filename:string;
-    private jsonObject: any = {};
+    private filename: string;
+    private jsonObject: any = [];
 
-    constructor(filename:string){
+    constructor(filename: string) {
         this.filename = filename;
     }
 
     /**
      * leer
      */
-     public async leer() {
+    public async leer() {
         try {
             const contenido = await fs.promises.readFile(this.filename, 'utf-8');
             this.jsonObject = JSON.parse(contenido);
             console.log(this.jsonObject);
+            
         } catch (error) {
             console.error(error);
         }
@@ -30,30 +31,29 @@ export class Archivo {
         const chance = new Chance();
         const newElement = {
             title: chance.animal(),
-            price: Math.ceil(Math.random()*1000),
-            thimbmail: chance.avatar(),
+            price: Math.ceil(Math.random() * 1000),
+            thumbmail: chance.avatar(),
             id: this.jsonObject.length + 1
         }
         this.jsonObject.push(newElement);
 
         try {
-            await fs.promises.writeFile(this.filename, 
-                JSON.stringify(this.jsonObject, null, '/t'), 'utf-8');
+            await fs.promises.writeFile(this.filename,
+                JSON.stringify(this.jsonObject, null, '\t'), 'utf-8');
         } catch (error) {
             console.error(error);
         }
-        
+
     }
 
-    
+
     /**
      * borrar
      */
     public async borrar() {
-        try{
+        try {
             await fs.promises.unlink(this.filename);
-        }catch (error)
-        {
+        } catch (error) {
             console.error(error);
         }
     }
--- sesion6/index.ts
+++ sesion6/index.ts
@@ -1,15 +1,17 @@
-const loadFile = () => {
-    import('./Archivo')
-        .then(module => {
-            const archivo = new module.Archivo('productos.txt')
-            archivo.leer();
-            archivo.guardar();
-            setTimeout(() => {
-                console.log("Borrando archivo...")
-                archivo.borrar();
-            }, 5000)
-        })
-        .catch(error => console.log(error))
+const loadFile = async () => {
+    const module = await import('./Archivo');
+    const archivo = new module.Archivo('productos.txt')
+    try {
+        await archivo.leer();
+        await archivo.guardar();
+        setTimeout(() => {
+            console.log("Borrando archivo...")
+            archivo.borrar();
+        }, 5000)
+    } catch(error){
+        console.error(error);
+    }
+
 }
 
 loadFile();
