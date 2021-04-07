dc7ac06b8928f21ae01856d08ec8f3364ec42fd3
nosemiusername <rafael@imatec.cl>
2021-04-07 00:18:09 -0400

    eliminar proyecto con problemas para transpilar js

--- session6/escritusra.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-
-fs.promises.readFile('./package.2json', 'utf-8').then( data => 
-    console.log(JSON.parse(data)))
-    .catch( err => {
-        console.log(err);
-    });
