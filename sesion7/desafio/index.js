af1f6889ecb817b8aa0286f177a0545488a86124
nosemiusername <rafael@imatec.cl>
2021-04-08 02:04:37 -0400

    se agregan mas productos y se corrige la carga de elemntos

--- sesion7/desafio/index.js
+++ sesion7/desafio/index.js
@@ -13,19 +13,39 @@ const server = app.listen(8080, () => {
     console.log(`${server.address().port}`);
 });
 
+app.get('/', (req, res) => {
+    res.json(0);
+});
 
-app.get('/items', (req, res) => {
-    try{
-      const items = await fs.promises.readFile('productos.txt','utf-8');  
-    } catch(error){
+app.get('/items', async (req, res) => {
+    try {
+        const items = await fs.promises.readFile('productos.txt', 'utf-8');
+        const countElements = {};
+        JSON.parse(items).map(element => element.title).
+            forEach(value => countElements[value] = (countElements[value] || 0) + 1);
+        res.json(countElements)
+    } catch (error) {
         console.error(error);
+        res.json(error);
     }
     ++visitas.visitas.ruta1;
-    res.json(0)})
-app.get('/item-random', (req, res) => { 
+})
+app.get('/item-random', async (req, res) => {
+    try {
+        const items = await fs.promises.readFile('productos.txt', 'utf-8');
+        const jsonItems = JSON.parse(items);
+        const randomNumber = Math.floor(Math.random()*jsonItems.length)
+        const itemsWithTitle = jsonItems.map(element => element.title);
+        const returnedObject = { item: itemsWithTitle[randomNumber]}
+        res.json(returnedObject);
+    } catch (error) {
+        console.error(error);
+        res.json(error);
+    }
     ++visitas.visitas.ruta2;
-    res.json(0)})
-app.get('/visitas', (req, res) => { 
-    res.json(visitas)})
-server.on("error", error => { console.error(error)})
+})
+app.get('/visitas', (req, res) => {
+    res.json(visitas)
+})
+server.on("error", error => { console.error(error) })
 
