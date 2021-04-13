2b5966f9571a4616f90ac7dab88202caf21c3fda
nosemiusername <rafael@imatec.cl>
2021-04-13 00:35:35 -0400

    desafio

---
+++ sesion8/desafio/Producto.ts
@@ -0,0 +1,31 @@
+export class Producto{
+    private _list:any =[]
+
+    /**
+     * obtener todos los productos
+     */
+    public list(){
+        return this._list;
+    }
+
+    /**
+     * agregar un nuevo producto
+     */
+    public addList(value:any){
+        value.id = (this._list.length||0)+1;
+        this._list.push(value);
+    }
+
+    public constructor(){
+
+    }
+
+    /**
+     * 
+     * @param value: id del producto que se quiere retornar 
+     * @returns producto
+     */
+    public getProductById(value:number){
+        return this._list.filter( (element: { id: number; }) => element.id === value)[0];
+    }
+}
