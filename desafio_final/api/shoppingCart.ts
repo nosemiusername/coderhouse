afee0946742d8a2f0ba4f9bdfa289493cc07039c
nosemiusername <rafael@imatec.cl>
2021-05-12 00:40:59 -0400

    upload shoppingCart

--- desafio_final/api/shoppingCart.ts
+++ desafio_final/api/shoppingCart.ts
@@ -1,3 +1,51 @@
-export class Carrito {
-    
+import { Product } from './product';
+import { ProductList } from './productList';
+export class ShoppingCart {
+    private _list: Array<Product> = [];
+    static _instance: ShoppingCart;
+
+    public static get Instance() {
+        return this._instance || (this._instance = new this())
+    }
+
+    public get list(): Array<Product> {
+        return this._list;
+    }
+
+    public getSize(): number {
+        return this._list.length;
+    }
+
+    public addProduct(idProduct: string) {
+        const producto = this.getByProductListId(idProduct)[0];
+        if (producto) {
+            this._list.push(producto);
+        } else {
+            throw Error('Id not found');
+        }
+    }
+
+    public getByProductListId(id: string, equal: boolean = true): Array<Product> {
+        const productList = ProductList.Instance;
+        return productList.list.filter(element => equal ? element.id == parseInt(id) : element.id != parseInt(id));
+    }
+
+    public getById(id: string, equal: boolean = true): Array<Product> {
+        return this._list.filter(element => equal ? element.id == parseInt(id) : element.id != parseInt(id));
+    }
+
+    public removeProduct(id: string) {
+        const productToRemove = this.getById(id)[0];
+        if (productToRemove) {
+            this._list = this.getById(id, false);
+            return 1;
+        } else {
+            throw new Error(`Id not found`);
+        }
+
+    }
+
+    public constructor() {
+
+    }
 }
--- desafio_final/router/shoppingCartRoutes.ts
+++ desafio_final/router/shoppingCartRoutes.ts
@@ -1,12 +1,43 @@
 import express, { Router } from 'express';
-import { Carrito } from '../api/shoppingCart'
-export const shoppingCartRouter:Router = express.Router();
+import { ProductList } from '../api/productList';
+import { Product } from '../api/product';
+import { isAdmin } from '../helper/functions';
 
-shoppingCartRouter.get('', () => {
+export const shoppingCartRouter: Router = express.Router();
+shoppingCartRouter.use(express.json());
+shoppingCartRouter.use(express.urlencoded({ extended: false }));
+
+const productList: ProductList = ProductList.Instance;
+
+shoppingCartRouter.get('', (req, res) => {
+    res.json(productList.list);
+});
+
+shoppingCartRouter.get('/:id/show', (req, res) => {
+    const product = productList.getById(req.params.id)[0];
+    if (product) {
+        res.status(200).json(product);
+    } else {
+        res.status(404).json("Product not found");
+    }
 
 });
-shoppingCartRouter.get('/:id/show', () => {});
-shoppingCartRouter.post('/', () => {});
-shoppingCartRouter.put('/:id', () => {});
-shoppingCartRouter.delete('/:id', () => {});
 
+shoppingCartRouter.post('/', (req, res) => {
+    //TO-DO: implement schema validatiom https://stackoverflow.com/questions/44755315/spread-operator-form-submission-in-express-js
+    const idProduct: string = req.body.id;
+    productList.addProduct(idProduct);
+    res.status(200).json("Created");
+
+});
+
+shoppingCartRouter.delete('/:id', (req, res) => {
+    try {
+        productList.removeProduct(req.params.id);
+        res.status(200).json("Deleted");
+    } catch (e) {
+        res.status(404).json(e.message);
+    }
+}
+
+);
