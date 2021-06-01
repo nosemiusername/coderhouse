815765339722173306b98601b2c497872b27a0cb
nosemiusername <rafael@imatec.cl>
2021-06-01 02:17:38 -0400

    init upload

---
+++ sesion21/desafio/config/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+export default {
+    port: process.env.PORT,
+    sqlite: {
+        client: process.env.SQLITE_CLIENT,
+        connection: { 
+            filename: process.env.SQLITE_FILENAME 
+        },
+        useNullAsDefault: process.env.SQLITE_UNAD 
+    },
+    mongoURI: process.env.MONGO_URI,
+    flagDB: process.env.FLAG_DB
+}
