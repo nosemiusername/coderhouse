205c9016bcb6fba218427c4f84066015b1788bae
nosemiusername <rafael@imatec.cl>
2021-06-06 16:38:24 -0400

    init comit

---
+++ sesion22/desafio/router/item.routes.js
@@ -0,0 +1,12 @@
+import express, { Router } from 'express';
+import ItemController from '../controllers/item.controller.js';
+
+export const itemRoute = Router();
+itemRoute.use(express.json());
+const result = {};
+
+itemRoute.put('/:id', ItemController.apiUpdateItem);
+itemRoute.delete('/:id', ItemController.apiDeleteItem);
+itemRoute.post('/add', ItemController.apiCreateItem);
+itemRoute.get('/list', ItemController.apiGetAllItems);
+itemRoute.get('/:id/show', ItemController.apiGetItemsByID);
