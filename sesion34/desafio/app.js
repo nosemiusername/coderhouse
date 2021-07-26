90d68f39c4c9120a5632742470f866a9c5cc1d40
nosemiusername <rafael@imatec.cl>
2021-07-26 14:23:01 -0400

    redo from project sample

---
+++ sesion34/desafio/app.js
@@ -0,0 +1,91 @@
+// Include the cluster module
+var cluster = require('cluster');
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for terminating workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the terminated workers
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+    // Code to run if we're in a worker process
+} else {
+    const __dirname = process.cwd();
+
+    var AWS = require('aws-sdk');
+    var express = require('express');
+    var bodyParser = require('body-parser');
+
+    AWS.config.region = process.env.REGION
+
+    var sns = new AWS.SNS();
+    var ddb = new AWS.DynamoDB();
+
+    var ddbTable = process.env.STARTUP_SIGNUP_TABLE;
+    var snsTopic = process.env.NEW_SIGNUP_TOPIC;
+    var app = express();
+
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.get('/', function (req, res) {
+        res.sendFile(`${__dirname}/src/public/index.html`);
+    });
+
+    app.get('/main.css', function (req, res) {
+        res.sendFile(`${__dirname}/src/public/main.css`);
+    });
+
+    app.post('/save', function (req, res) {
+        const item = req.body
+        ddb.putItem({
+            'TableName': ddbTable,
+            'Item': item,
+            'Expected': { email: { Exists: false } }
+        }, function (err, data) {
+            if (err) {
+                var returnStatus = 500;
+
+                if (err.code === 'ConditionalCheckFailedException') {
+                    returnStatus = 409;
+                }
+
+                res.status(returnStatus).end();
+                console.log('DDB Error: ' + err);
+            } else {
+                sns.publish({
+                    'Message': `Item detail: ${item}`,
+                    'Subject': 'New product!',
+                    'TopicArn': snsTopic
+                }, function (err, data) {
+                    if (err) {
+                        res.status(500).end();
+                        console.log('SNS Error: ' + err);
+                    } else {
+                        res.status(201).end();
+                    }
+                });
+            }
+        });
+    });
+
+    var port = process.env.PORT || 3000;
+
+    var server = app.listen(port, function () {
+        console.log('Server running at http://127.0.0.1:' + port + '/');
+    });
+}
--- sesion34/desafio/src/config/index.js
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-
-/** Loading from console if there is parameter */
-export const config = {
-    port: process.env.PORT || 3000,
-}
