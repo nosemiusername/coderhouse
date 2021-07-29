339fb1682872e813a8906f2b0ddf83e07989cf9c
nosemiusername <rafael@imatec.cl>
2021-07-29 00:55:09 -0400

    adding configs

--- sesion36/desafio/src/app.js
+++ sesion36/desafio/src/app.js
@@ -3,12 +3,16 @@ import { webRouter } from './routes/webroute.js'
 import config from './config/index.js';
 import { load } from './loader/index.js';
 import { info } from './config/logger.js'
+import passport from 'passport';
+
 
 load();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', webRouter);
 
 app.listen(config.port, () => {
--- sesion36/desafio/src/routes/webroute.js
+++ sesion36/desafio/src/routes/webroute.js
@@ -6,7 +6,7 @@ import { UserController } from '../controllers/UserController.js';
 export const webRouter = Router();
 const __dirname = process.cwd();
 
-passport.use('login', new LocalStrategy(async (email, password, done) => {
+passport.use('login', new LocalStrategy({ passReqToCallback: true }, async (req, email, password, done) => {
     const user = await UserController.find(email, password);
     if (user) {
         return done(null, user);
@@ -15,7 +15,7 @@ passport.use('login', new LocalStrategy(async (email, password, done) => {
     }
 }))
 
-passport.use('signup', new LocalStrategy(async (name, email, password, age, address, done) => {
+passport.use('signup', new LocalStrategy({ passReqToCallback: true }, async (req, done) => {
     const user = await UserController.create(name, email, password, age, address);
     if (user) {
         return done(null, user);
@@ -24,12 +24,13 @@ passport.use('signup', new LocalStrategy(async (name, email, password, age, addr
     }
 }))
 
-webRouter.post('/register', passport.authenticate('signup', { failureRedirect: '/signup.html' }), (req, res, next) => {
-    res.redirect('/');
+webRouter.post('/register', passport.authenticate('signup', { failureRedirect: '/failregister' }), (req, res, next) => {
+    res.redirect('/failregister');
 }
 );
 
-webRouter.post('/login', passport.authenticate('signup', { failureRedirect: '/login.html' }), (req, res, next) => {
+// webRouter.post('/register', passport.authenticate('signup', {failureRedirect:'/failregister', successRedirect:'/'}));
+webRouter.post('/login', passport.authenticate('login', { failureRedirect: '/failregister' }), (req, res, next) => {
     res.redirect('/');
 }
 );
@@ -48,3 +49,12 @@ webRouter.get('/main.css', (req, res, next) => {
 webRouter.get('/', (req, res, next) => {
     res.sendFile(`${__dirname}/src/public/login.html`);
 });
+
+webRouter.get('/fail', (req, res, next) => {
+    res.json('fail');
+});
+
+
+webRouter.post('/register2', (req, res, next) => {
+    res.status(200).json();
+})
