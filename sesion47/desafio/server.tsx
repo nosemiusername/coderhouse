8f8459b4392477faeac05382de21126880233092
nosemiusername <rafael@imatec.cl>
2021-09-16 01:18:16 -0300

    added li eleemnt

--- sesion47/desafio/server.tsx
+++ sesion47/desafio/server.tsx
@@ -1,4 +1,7 @@
-import { createApp } from "https://deno.land/x/servest@v1.3.1/mod.ts";
+import {
+  contentTypeFilter,
+  createApp,
+} from "https://deno.land/x/servest@v1.3.1/mod.ts";
 
 // @deno-types="https://deno.land/x/servest@v1.3.1/types/react/index.d.ts"
 import React from "https://dev.jspm.io/react/index.js";
@@ -6,41 +9,53 @@ import React from "https://dev.jspm.io/react/index.js";
 // @deno-types="https://deno.land/x/servest@v1.3.1/types/react-dom/server/index.d.ts"
 import ReactDOMServer from "https://dev.jspm.io/react-dom/server.js";
 
+// @deno-types="https://deno.land/x/servest@v1.3.1/types/react-dom/server/index.d.ts"
+import ReactDOM from "https://dev.jspm.io/react-dom/server.js";
+
 const app = createApp();
 
-const colors = [];
+const colors: any = [];
+
+const App = () => colors.map((item: string, key: string) => <li>{item}</li>);
 
 //TODO: Implementar carga de listado de colors
-app.handle("/", async (req) => {
+app.get("/", async (req) => {
   await req.respond({
     status: 200,
     headers: new Headers({
       "content-type": "text/html; charset=UTF-8",
     }),
-    body: ReactDOMServer.renderToString(
-      <html>
-        <head></head>
-        <body>
-          <form action="/addColor" method="POST">
-            <input
-              class="input"
-              type="text"
-              name="color"
-              id="color"
-              placeholder="Insertar Color"
-            />
-            <button style="display:block" type="submit">
-              Enviar
-            </button>
-          </form>
-          <div id="divColor"></div>
-        </body>
-      </html>
-    ),
+    body: `
+    <html>
+    <head></head>
+    <body>
+      <form action="/addColor" method="POST">
+        <input
+          type="text"
+          name="color"
+          id="color"
+          placeholder="Insertar Color"
+        />
+        <button type="submit">Enviar</button>
+      </form>
+      <ul style="background-color:grey" id="ul">${ReactDOMServer.renderToString(
+        <App />
+      )}</ul>
+    </body>
+  </html>`,
   });
 });
 
 //TODO: Implementar redirect de /
-app.handle("/addColor", async (req) => {});
+app.post(
+  "/addColor",
+  contentTypeFilter("application/x-www-form-urlencoded"),
+  async (req) => {
+    const bodyForm = await req.formData();
+    const color = bodyForm.value("color");
+    colors.push(color);
+    await req.redirect("/");
+  }
+);
 
 app.listen({ port: 8080 });
